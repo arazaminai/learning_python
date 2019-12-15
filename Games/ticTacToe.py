0e23bdb5aff74e14a1aec12e296fb40b429ae9e5
Raza Minai <kingminai3@gmail.com>
2019-12-15 23:30:08 +0200

    added things

--- Games/ticTacToe.py
+++ Games/ticTacToe.py
@@ -19,20 +19,9 @@ def player():
             else:
                 print("Between 1-9, as shown in the diagram")
 
-def game():
-    
-
-while True:
-    the_board = {
-        1: "1", 2: "2", 3: "3",
-        4: "4", 5: "5", 6: "6",
-        7: "7", 8: "8", 9: "9"
-        }
-    score_x = 0
-    score_y = 0
+def multiplayer():
     turn = "X"
-
-    for i in range(9):
+    while True:
         #break
         print_board(the_board)
         print("Turn for " + turn + ". Move on which space?")
@@ -43,7 +32,40 @@ while True:
             turn = "O"
         else:
             turn = "X"
+        if game(the_board):
+            print
+            break
     
+def game(board):
+    if ("X" in (board[1] and board[2] and board[3])) or ("X" in (board[4] and board[5] and board[6])) or ("X" in (board[7] and board[8] and board[9])):
+        print("X Wins!!!")
+        return #score_x + 1
+    elif ("X" in (board[1] and board[4] and board[7])) or ("X" in (board[2] and board[5] and board[8])) or ("X" in (board[3] and board[6] and board[9])):
+        print("X Wins!!!")
+        return #score_x + 1
+    elif ("X" in (board[1] and board[5] and board[9])) or ("X" in (board[7] and board[5] and board[3])):
+        print("X Wins!!!")
+        return #score_x + 1
+
+    elif ("O" in (board[1] and board[2] and board[3])) or ("O" in (board[4] and board[5] and board[6])) or ("O" in (board[7] and board[8] and board[9])):
+        print("O Wins!!!")
+        return #score_o + 1
+    elif ("O" in (board[1] and board[4] and board[7])) or ("O" in (board[2] and board[5] and board[8])) or ("O" in (board[3] and board[6] and board[9])):
+        print("O Wins!!!")
+        return #score_o + 1
+    elif ("O" in (board[1] and board[5] and board[9])) or ("O" in (board[7] and board[5] and board[3])):
+        print("O Wins!!!")
+        return #score_o + 1
+
+while True:
+    the_board = {
+        1: "1", 2: "2", 3: "3",
+        4: "4", 5: "5", 6: "6",
+        7: "7", 8: "8", 9: "9"
+        }
+    score_x = 0
+    score_y = 0
+    multiplayer()
     print_board(the_board)
 
     while True:
