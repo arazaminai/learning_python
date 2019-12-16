5e983ecc7347e5dcc01f8ad5675cfd89d09c7983
Raza Minai <kingminai3@gmail.com>
2019-12-16 02:18:01 +0200

    cleaned up code

--- Games/ticTacToe.py
+++ Games/ticTacToe.py
@@ -1,37 +1,42 @@
-import sys
+import sys, random
 
 def print_board(board):
-    print(board[1] + "|" + board[2] + "|" + board[3])
+    print(board[7] + "|" + board[8] + "|" + board[9])
     print("-+-+-")
     print(board[4] + "|" + board[5] + "|" + board[6])
     print("-+-+-")
-    print(board[7] + "|" + board[8] + "|" + board[9])
+    print(board[1] + "|" + board[2] + "|" + board[3])
 
-def player(): 
+def player(turn): 
     while True:
-        move = input()
-        if move == "":
-            print()
-        else:
-            move = int(move)
-            if move in the_board.keys():
-                return move
-            else:
-                print("Between 1-9, as shown in the diagram")
+        try:
+            move = input()
+            if move != "":
+                move = int(move)
+                if move in the_board.keys():
+                    return move
+                else:
+                    print("Error: Only between 1-9, as shown in the diagram")
+                    print("Turn for " + turn + ". Move on which space?")
+        except ValueError:
+            print("Error: Only whole-numbers betweem 1-9, as shown in the diagram")
+            print("Turn for " + turn + ". Move on which space?")
 
 def multiplayer():
     turn = "X"
+    
     while True:
-        #break
         print("Turn for " + turn + ". Move on which space?")
-        #move = input()
-        move = player()
+        move = player(turn)
         the_board[move] = turn
+        
         if turn == "X":
             turn = "O"
         else:
             turn = "X"
+        
         print_board(the_board)
+
         if game(the_board):
             break
     
@@ -41,39 +46,45 @@ def game(board):
         return True
     if "X" in board[1] and "X" in board[4] and "X" in board[7] or "X" in board[2] and "X" in board[5] and "X" in board[8] or "X" in board[3] and "X" in board[6] and "X" in board[9]:
         print("X Wins!!!")
-        return #score_x + 1
+        return True
     if "X" in board[3] and "X" in board[5] and "X" in board[7] or "X" in board[1] and "X" in board[5] and "X" in board[9]:
         print("X Wins!!!")
-        return #score_x + 1
+        return True
 
     if "O" in board[1] and "O" in board[2] and "O" in board[3] or "O" in board[4] and "O" in board[5] and "O" in board[6] or "O" in board[7] and "O" in board[8] and "O" in board[9]:
         print("O Wins!!!")
-        return #score_o + 1
+        return True
     if "O" in board[1] and "O" in board[4] and "O" in board[7] or "O" in board[2] and "O" in board[5] and "O" in board[8] or "O" in board[3] and "O" in board[6] and "O" in board[9]:
         print("O Wins!!!")
-        return #score_o + 1
+        return True
     if "O" in board[3] and "O" in board[5] and "O" in board[7] or "O" in board[1] and "O" in board[5] and "O" in board[9]:
         print("O Wins!!!")
-        return #score_o + 1
+        return True
 
 while True:
-    the_board = {
-        1: "1", 2: "2", 3: "3",
-        4: "4", 5: "5", 6: "6",
-        7: "7", 8: "8", 9: "9"
-        }
-    score_x = 0
-    score_o = 0
-    print_board(the_board)
-    multiplayer()
+    try:
+        the_board = {
+            7: "7", 8: "8", 9: "9",
+            4: "4", 5: "5", 6: "6",
+            1: "1", 2: "2", 3: "3"
+            }
 
-    while True:
-        play = input("Play again?(y/n): ")
-        if play == "n":
-            sys.exit()
-        elif play == "y":
-            break
+        score_x = 0
+        score_o = 0
+        
+        print_board(the_board)
+        
+        multiplayer()
 
+        while True:
+            play = input("Play again?(y/n): ")
+            
+            if play == "n":
+                sys.exit()
+            elif play == "y":
+                break
+    except KeyboardInterrupt:
+        sys.exit()
 
 
 
